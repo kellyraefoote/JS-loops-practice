e466771eb644b41cd618f57347ef23d681276a06
Kelly F <95229425+kellyraefoote@users.noreply.github.com>
2023-12-30 20:00:34 -0800

    Inital Commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// *Variables*
+// Declare a variable, assign it a boolean, and alert the value
+let haveADuck = true
+alert(haveADuck)
+// Declare a variable, reassign it to your favorite color, and console log the value
+let favoriteColor = "red" 
+favoriteColor = "blue"
+console.log(favoriteColor)
+
+// *Functions*
+// Create a function that takes in 4 numbers and returns the sum of the first 3 numbers divided by the fourth. Return the result. Call the function.
+function sumThreeAndDivide(n1,n2,n3,n4){
+    return(n1+n2+n3)/n4
+
+}
+sumThreeAndDivide(10,20,30,5)
+// Create a function that takes in 2 numbers. Console log the first number to the power of the second. Call the function.
+function mathStuff(n1, n2) {
+    console.log(Math.pow(n1,n2))
+}
+mathStuff(5,2)
+// *Conditionals*
+// Create a function that takes in a boolean and a string. If the boolean is true, alert the string. If the boolean is false, console log the string
+function alertOrLog(b, str) {
+    if (b) {
+        alert(str)
+    } else {
+        console.log(str)
+    }
+}
+//alertOrLog(true, "I should be an alert")
+alertOrLog(false, "I should be in the console")
+
+//*Loops*
+//Create a function that takes in a number. Console log all values from 1 to that number, but if the number is divisible by 3 log "fizz" instead of that number, if the number is divisible by 5 log "buzz" instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
+function fizzbuzz (num1) {
+    for(let i=1; i <= num1; i++){
+
+    if (i%3 === 0 && i%5 ===0) {
+        console.log("fizzbuzz")
+    } else if (i%3 === 0) {
+        console.log ("fizz")
+    } else if  (i%5 === 0) {
+        console.log("buzz") 
+    } else {
+        console.log(i)
+    }
+    }
+}
+fizzbuzz(100)
+
+
